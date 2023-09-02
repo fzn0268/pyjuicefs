8a2cf9e835508687b098ef00847ca8bb499668fb
fzn0268 <fzn0268@users.noreply.github.com>
2023-09-02 09:58:49 +0000

    ✅ Ready to clone and code.

---
+++ pyjuicefs/__main__.py
@@ -0,0 +1,6 @@
+"""Entry point for pyjuicefs."""
+
+from pyjuicefs.cli import main  # pragma: no cover
+
+if __name__ == "__main__":  # pragma: no cover
+    main()
--- tests/test_base.py
+++ tests/test_base.py
@@ -1,5 +1,5 @@
-from project_name.base import NAME
+from pyjuicefs.base import NAME
 
 
 def test_base():
-    assert NAME == "project_name"
+    assert NAME == "pyjuicefs"
